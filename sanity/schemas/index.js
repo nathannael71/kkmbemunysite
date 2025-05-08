657fc2e4b57ef1fdc600f906d01e5d2bf73b40fb
Nathan <88581278+nathannael71@users.noreply.github.com>
2025-05-08 21:51:27 +0700

    Create index.js

---
+++ sanity/schemas/index.js
@@ -0,0 +1,21 @@
+import article from './article'
+import author from './author'
+import gallery from './gallery'
+import hero from './hero'
+import program from './program'
+import team from './team'
+import timeline from './timeline'
+import about from './about'
+import siteSettings from './siteSettings'
+
+export const schemaTypes = [
+  article,
+  author,
+  gallery,
+  hero,
+  program,
+  team,
+  timeline,
+  about,
+  siteSettings
+]
